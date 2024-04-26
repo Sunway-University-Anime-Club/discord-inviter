854cc194f6669ca4ec843a301e09a6bca7e86338
Spimy <officialspyme@gmail.com>
2024-04-27 03:41:38 +0800

    📦 NEW: Add layout with svelte toast

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { SvelteToast } from '@zerodevx/svelte-toast';
+</script>
+
+<main>
+	<div class="toast-wrapper"></div>
+	<SvelteToast
+		options={{
+			duration: 5000,
+			theme: {
+				'--toastBarBackground': 'orange',
+				'--toastBackground': 'white',
+				'--toastColor': 'black'
+			},
+			pausable: true
+		}}
+	/>
+	<slot />
+</main>
+
+<style>
+	main {
+		font-family: Arial, Helvetica, sans-serif;
+	}
+</style>
