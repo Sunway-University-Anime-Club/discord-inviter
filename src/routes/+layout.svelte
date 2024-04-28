a1c1e2a9e3e841fef35c16d8bd8fd0d8ccfc5e3f
Spimy <officialspyme@gmail.com>
2024-04-29 00:46:34 +0800

    👌 IMPROVE: Move toast to bottom of page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,23 +3,44 @@
 </script>
 
 <main>
-	<SvelteToast
-		options={{
-			duration: 5000,
-			theme: {
-				'--toastBarBackground': 'orange',
-				'--toastBackground': 'white',
-				'--toastColor': 'black'
-			},
-			pausable: true
-		}}
-	/>
+	<div class="toast-wrapper">
+		<SvelteToast
+			options={{
+				duration: 5000,
+				pausable: true,
+				reversed: true,
+				intro: { y: 192 }
+			}}
+		/>
+	</div>
 	<slot />
 </main>
 
 <style>
 	main {
 		font-family: Arial, Helvetica, sans-serif;
+	}
+
+	main .toast-wrapper {
+		--toastContainerTop: auto;
+		--toastContainerRight: 1rem;
+		--toastContainerBottom: 8rem;
+		--toastContainerLeft: 1rem;
 		--toastWidth: 100%;
+		--toastMinHeight: 2rem;
+		--toastPadding: 0 0.5rem;
+		--toastBorderRadius: 0.2rem;
+		--toastBarBackground: var(--secondary-clr);
+		--toastBackground: white;
+		--toastColor: black;
+		font-size: 0.875rem;
+	}
+
+	@media (min-width: 48em) {
+		main .toast-wrapper {
+			--toastContainerRight: auto;
+			--toastContainerLeft: calc(50vw - 20rem);
+			--toastWidth: 40rem;
+		}
 	}
 </style>
